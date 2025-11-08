e076c4640fbcd8f628bf9a1040637f6fd54d1757
Sammy <arianandana043@gmail.com>
2025-11-08 13:36:18 +0800

    main

---
+++ resources/views/components/app-logo-icon.blade.php
@@ -0,0 +1,1 @@
+<img {{ $attributes }} src="{{ asset('storage/logo/logo baleganjur.png') }}" alt="Warung Baleganjur Logo">
---
+++ resources/views/components/app-logo.blade.php
@@ -0,0 +1,6 @@
+{{-- <div class="flex ">
+    <x-app-logo-icon class="size-10 rounded-md"/>
+</div> --}}
+<div class="ms-1 grid flex-1 text-start text-sm">
+    <span class="mb-0.5 truncate leading-tight font-semibold">Warung Baleganjur</span>
+</div>
